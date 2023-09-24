ae2d25162f2a3c2fda10b9aa73696615e5790eb6
Ashutosh Punyani <ashutoshspunyani99@tamu.edu>
2023-09-23 21:32:38 -0500

    init_paging and PageTableConstructor definition added

--- MP3/page_table.C
+++ MP3/page_table.C
@@ -16,14 +16,42 @@ void PageTable::init_paging(ContFramePool * _kernel_mem_pool,
                             ContFramePool * _process_mem_pool,
                             const unsigned long _shared_size)
 {
-   assert(false);
-   Console::puts("Initialized Paging System\n");
+   // initlizaing basic data structure for paging
+   Console::puts("Initialized Paging System Start\n");
+
+   kernel_mem_pool = _kernel_mem_pool;
+   process_mem_pool = _process_mem_pool;
+   shared_size = _shared_size;
+
+   Console::puts("Initialized Paging System End\n");
 }
 
 PageTable::PageTable()
 {
-   assert(false);
-   Console::puts("Constructed Page Table object\n");
+   Console::puts("Constructed Page Table object Start\n");
+   unsigned long page_directory_frame_number = kernel_mem_pool->get_frames(1);
+   page_directory = (unsigned long *)(page_directory_frame_number * PAGE_SIZE);
+
+   unsigned long page_table_frame_number = kernel_mem_pool->get_frames(1);
+   unsigned long *page_table = (unsigned long*)(page_table_frame_number * PAGE_SIZE);
+
+  
+
+   // mappng the first 4MB of memory
+   for(unsigned int i = 0, physical_address = 0; i < ENTRIES_PER_PAGE; i++,physical_address + PAGE_SIZE)
+   { 
+      page_table[i] = physical_address | 0x3; // attribute set to: supervisor level, read/write, present(011 in binary)
+   }
+
+   page_directory[0] = page_table; // attribute set to: supervisor level, read/write, present(011 in binary)
+   page_directory[0] = page_directory[0] | 0x3;
+
+   for(unsigned int i = 1; i < ENTRIES_PER_PAGE; i++)
+   {
+      page_directory[i] = 0 | 0x2; // attribute set to: supervisor level, read/write, not present(010 in binary)
+   }
+
+   Console::puts("Constructed Page Table object End\n");
 }
 
 
