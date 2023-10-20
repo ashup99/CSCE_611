8b66827de03218b1eb2d3046400edd1502504b8b
Ashutosh Punyani <ashutoshspunyani99@tamu.edu>
2023-10-20 03:47:56 -0500

    vm pool changes added

--- MP4/vm_pool.C
+++ MP4/vm_pool.C
@@ -1,8 +1,8 @@
 /*
  File: vm_pool.C
  
- Author:
- Date  :
+ Author: Ashutosh Punyani
+ Date  : October,20 2023
  
  */
 
@@ -59,35 +59,34 @@ VMPool::VMPool(unsigned long  _base_address,
     available_size=_size;
     total_count=0;
 
+    page_table->register_pool(this);
 
     // using the first pool to store region data
     region_data *temp_region = (region_data*) base_address;
     temp_region[0].base_addr = base_address;
-    temp_region[0].size = PAGE_SIZE;
-    last_region_end_address = base_address + PAGE_SIZE;
+    temp_region[0].size = PageTable::PAGE_SIZE;
     regions=temp_region;
-    available_size -= PAGE_SIZE;
+    available_size -= PageTable::PAGE_SIZE;
     total_count+=1;
 
-    page_table->register_pool(this);
-
     Console::puts("Constructed VMPool object - end.\n");
+
 }
 
 unsigned long VMPool::allocate(unsigned long _size) {
     // assert(false);
     Console::puts("Allocated region of memory - start. \n");
     if(available_size<_size){
-        Console::puts("No free size available.");
+        Console::puts("No free size available.\n");
         assert(false);
     }
-    unsigned number_of_pages = _size/PAGE_SIZE;
-    number_of_pages = _size%PAGE_SIZE>0?number_of_pages+1:number_of_pages
-    regions[total_count].base_addr=last_region_end_address;
-    regions[total_count].size= number_of_pages * PAGE_SIZE;
+    unsigned number_of_pages = _size/PageTable::PAGE_SIZE;
+    number_of_pages = _size%PageTable::PAGE_SIZE>0?number_of_pages+1:number_of_pages;
+    regions[total_count].base_addr=regions[total_count-1].base_addr+regions[total_count-1].size;
+    regions[total_count].size= number_of_pages * PageTable::PAGE_SIZE;
     total_count+=1;
-    available_size-=number_of_pages * PAGE_SIZE;
-    last_region_end_address+=number_of_pages * PAGE_SIZE;
+    available_size-=number_of_pages * PageTable::PAGE_SIZE;
+    
     Console::puts("Allocated region of memory - end.\n");
     return regions[total_count-1].base_addr;
 }
@@ -96,7 +95,7 @@ void VMPool::release(unsigned long _start_address) {
     // assert(false);
     Console::puts("Released region of memory - start.\n");
     unsigned long region_relase_index=-1;
-    for(unsigned long i =0 ;i<total_count;i++){
+    for(unsigned long i =1 ;i<total_count;i++){
         if(regions[i].base_addr==_start_address){
             region_relase_index=i;
             break;
@@ -108,23 +107,21 @@ void VMPool::release(unsigned long _start_address) {
     }
     else{
         unsigned long curr_addr= _start_address;
-        unsigned long number_of_pages=regions[region_relase_index].size/PAGE_SIZE;
+        unsigned long number_of_pages=regions[region_relase_index].size/PageTable::PAGE_SIZE;
         for(unsigned long i =0;i<number_of_pages;i++){
             page_table->free_page(curr_addr);
-            curr_addr+=PAGE_SIZE;
+            curr_addr+=PageTable::PAGE_SIZE;
         }
 
-        for(unsigned long i =region_relase_index;i<total_count-1;i++){
+        for(unsigned long i =region_relase_index;i<total_count;i++){
             regions[i]=regions[i+1];
         }
 
         total_count-=1;
         available_size+=regions[total_count].size;
 
-        if(regions[region_relase_index].base_addr+regions[region_relase_index].size==last_region_end_address){
-            last_region_end_address =regions[region_relase_index].base_addr;
-        }
-        page_table->load();
+        regions[region_relase_index].base_addr=regions[total_count].base_addr;
+        regions[region_relase_index].size=regions[total_count].size;
     }
     page_table->load();
     Console::puts("Released region of memory - end.\n");
@@ -134,13 +131,14 @@ bool VMPool::is_legitimate(unsigned long _address) {
     // assert(false);
     Console::puts("Checked whether address is part of an allocated region - start.\n");
     for(unsigned long i =0; i<total_count;i++){
-        if(_address>=regions[i].base_addr && _address<=regions[i].base_addr+regions[i].size){
-            Console::puts("Legitimate");
+        if(regions[i].base_addr<=_address && regions[i].base_addr+regions[i].size>=_address){
+            Console::puts("Legitimate.\n");
+            Console::puts("Checked whether address is part of an allocated region - end.\n");
             return true;
         }
     }
-    Console::puts("Not Legitimate");
-    return false;
+    Console::puts("Not Legitimate.\n");
     Console::puts("Checked whether address is part of an allocated region - end.\n");
+    return false;
 }
 
