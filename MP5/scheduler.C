eb7591a8490461636992a2b4d7ceea27abb0057c
Ashutosh Punyani <ashutoshspunyani99@tamu.edu>
2023-11-05 21:08:56 -0600

    formatted

--- MP5/scheduler.C
+++ MP5/scheduler.C
@@ -1,9 +1,9 @@
 /*
  File: scheduler.C
- 
+
  Author: Ashutosh Punyani
  Date  : Nov 5, 2023
- 
+
  */
 
 /*--------------------------------------------------------------------------*/
@@ -40,253 +40,289 @@
 /* FORWARDS */
 /*--------------------------------------------------------------------------*/
 
-extern MemPool* MEMORY_POOL;
+extern MemPool *MEMORY_POOL;
 
 /*--------------------------------------------------------------------------*/
 /* METHODS FOR CLASS   S c h e d u l e r  */
 /*--------------------------------------------------------------------------*/
 
-Scheduler::Scheduler() {
+Scheduler::Scheduler()
+{
   // assert(false);
-  head=NULL;
-  current=NULL;
+  head = NULL;
+  current = NULL;
   Console::puts("Constructed Scheduler::Scheduler().\n");
 }
 
-void Scheduler::yield() {
+void Scheduler::yield()
+{
   // assert(false);
   Console::puts("Scheduler::yield().\n");
 }
 
-void Scheduler::resume(Thread * _thread) {
+void Scheduler::resume(Thread *_thread)
+{
   // assert(false);
   Console::puts("Scheduler::resume().\n");
 }
 
-void Scheduler::add(Thread * _thread) {
+void Scheduler::add(Thread *_thread)
+{
   Console::puts("Scheduler::add().\n");
 }
 
-void Scheduler::terminate(Thread * _thread) {
+void Scheduler::terminate(Thread *_thread)
+{
   Console::puts("Scheduler::terminate().\n");
 }
 
-FIFOScheduler::FIFOScheduler() {
+FIFOScheduler::FIFOScheduler()
+{
   Console::puts("Constructed FIFOScheduler::FIFOScheduler() - start.\n");
-  head=NULL;
-  current=NULL;
+  head = NULL;
+  current = NULL;
   Console::puts("Constructed FIFOScheduler::FIFOScheduler() - end.\n");
 }
 
-void FIFOScheduler::yield() {
+void FIFOScheduler::yield()
+{
   // assert(false);
-  if(Machine::interrupts_enabled()){
+  if (Machine::interrupts_enabled())
+  {
     Console::puts("Interrupts Disabled.\n");
     Machine::disable_interrupts();
   }
   Console::puts("FIFOScheduler::yield() - start.\n");
-  Thread_List* temp=head;
-  if(head==NULL){
+  Thread_List *temp = head;
+  if (head == NULL)
+  {
     Console::puts("Empty Ready Queue\n");
     assert(false);
   }
-  if(head->next==NULL){
+  if (head->next == NULL)
+  {
     Console::puts("Before last Thread\n");
   }
-  head=head->next;
-  head->prev=NULL;
+  head = head->next;
+  head->prev = NULL;
   Console::puts("Thread Dispatched to : ");
-  Console::puti(temp->thread->ThreadId()+1);
+  Console::puti(temp->thread->ThreadId() + 1);
   Console::puts("\n");
   Thread::dispatch_to(temp->thread);
   MEMORY_POOL->release((unsigned long)temp);
   Console::puts("FIFOScheduler::yield() - end.\n");
- if(!Machine::interrupts_enabled()){
+  if (!Machine::interrupts_enabled())
+  {
     Console::puts("Interrupts Enabled.\n");
     Machine::enable_interrupts();
   }
 }
 
-void FIFOScheduler::add(Thread * _thread) {
+void FIFOScheduler::add(Thread *_thread)
+{
   // assert(false);
-  if(Machine::interrupts_enabled()){
+  if (Machine::interrupts_enabled())
+  {
     Console::puts("Interrupts Disabled.\n");
     Machine::disable_interrupts();
   }
   Console::puts("FIFOScheduler::add() - start.\n");
-  Thread_List* new_thread = (Thread_List*)(MEMORY_POOL->allocate(sizeof(Thread_List)));
-  new_thread->thread=_thread;
-  new_thread->next=NULL;
-  new_thread->prev=NULL;
-  if(head==NULL && current==NULL){
-    head=new_thread;
-    current=new_thread;
+  Thread_List *new_thread = (Thread_List *)(MEMORY_POOL->allocate(sizeof(Thread_List)));
+  new_thread->thread = _thread;
+  new_thread->next = NULL;
+  new_thread->prev = NULL;
+  if (head == NULL && current == NULL)
+  {
+    head = new_thread;
+    current = new_thread;
   }
-  else{
-    current->next=new_thread;
-    new_thread->prev=current;
-    current=new_thread;
+  else
+  {
+    current->next = new_thread;
+    new_thread->prev = current;
+    current = new_thread;
   }
   Console::puts("Thread Added : ");
-  Console::puti(_thread->ThreadId()+1);
+  Console::puti(_thread->ThreadId() + 1);
   Console::puts("\n");
   Console::puts("FIFOScheduler::add() - end.\n");
-  if(!Machine::interrupts_enabled()){
+  if (!Machine::interrupts_enabled())
+  {
     Console::puts("Interrupts Enabled.\n");
     Machine::enable_interrupts();
   }
 }
 
-void FIFOScheduler::resume(Thread * _thread) {
+void FIFOScheduler::resume(Thread *_thread)
+{
   // assert(false);
   Console::puts("FIFOScheduler::resume() - start.\n");
   add(_thread);
   Console::puts("Thread Resume:");
-  Console::putui(_thread->ThreadId()+1);
+  Console::putui(_thread->ThreadId() + 1);
   Console::puts("\n");
   Console::puts("FIFOScheduler::resume() - end.\n");
 }
 
-void FIFOScheduler::terminate(Thread * _thread) {
+void FIFOScheduler::terminate(Thread *_thread)
+{
   // assert(false);
-  if(Machine::interrupts_enabled()){
+  if (Machine::interrupts_enabled())
+  {
     Console::puts("Interrupts Disabled.\n");
     Machine::disable_interrupts();
   }
   Console::puts("FIFOScheduler::terminate() - start.\n");
-  if(Thread::CurrentThread()==_thread){
+  if (Thread::CurrentThread() == _thread)
+  {
     yield();
   }
-  else if (head->thread==_thread){
-    head=head->next;
-    head->prev=NULL;
+  else if (head->thread == _thread)
+  {
+    head = head->next;
+    head->prev = NULL;
   }
-  else{
-    Thread_List* itr =head;    
-     for(itr=head;itr->next->thread!=_thread;itr=itr->next)
-     {
+  else
+  {
+    Thread_List *itr = head;
+    for (itr = head; itr->next->thread != _thread; itr = itr->next)
+    {
+    }
 
-     }
+    if (itr != NULL)
+    {
 
-     if(itr!=NULL){
+      Thread_List *n_temp = itr->next;
+      Thread_List *p_temp = itr->prev;
+      itr->next = n_temp->next;
+      itr->prev = p_temp->prev;
 
-    Thread_List* n_temp = itr->next;    
-    Thread_List* p_temp = itr->prev;    
-    itr->next=n_temp->next;
-    itr->prev=p_temp->prev;
-
-  MEMORY_POOL->release((unsigned long)n_temp);
-  MEMORY_POOL->release((unsigned long)p_temp);
-     }
+      MEMORY_POOL->release((unsigned long)n_temp);
+      MEMORY_POOL->release((unsigned long)p_temp);
+    }
   }
   Console::puts("Thread Terminated : ");
-  Console::puti(_thread->ThreadId()+1);
+  Console::puti(_thread->ThreadId() + 1);
   Console::puts("\n");
   Console::puts("FIFOScheduler::terminate() - end.\n");
-  if(!Machine::interrupts_enabled()){
+  if (!Machine::interrupts_enabled())
+  {
     Console::puts("Interrupts Enabled.\n");
     Machine::enable_interrupts();
   }
 }
 
-RRScheduler::RRScheduler() {
+RRScheduler::RRScheduler()
+{
   Console::puts("Constructed RRScheduler::RRScheduler() - start.\n");
-  quantum_passed=false;
+  quantum_passed = false;
   Console::puts("Constructed RRScheduler::RRScheduler() - end.\n");
 }
 
-void RRScheduler::yield() {
+void RRScheduler::yield()
+{
   Console::puts("RRScheduler::yield() - start.\n");
-  if(quantum_passed){
-    quantum_passed=false;
+  if (quantum_passed)
+  {
+    quantum_passed = false;
   }
 
-  Thread_List* temp=head;
-  if(head==NULL){
+  Thread_List *temp = head;
+  if (head == NULL)
+  {
     Console::puts("Empty Ready Queue\n");
     assert(false);
   }
-  if(head->next==NULL){
+  if (head->next == NULL)
+  {
     Console::puts("Before last Thread\n");
   }
-  head=head->next;
-  head->prev=NULL;
+  head = head->next;
+  head->prev = NULL;
   Console::puts("Thread Dispatched to : ");
-  Console::puti(temp->thread->ThreadId()+1);
+  Console::puti(temp->thread->ThreadId() + 1);
   Console::puts("\n");
   Thread::dispatch_to(temp->thread);
   MEMORY_POOL->release((unsigned long)temp);
   Console::puts("RRScheduler::yield() - end.\n");
 }
 
-void RRScheduler::add(Thread * _thread) {
+void RRScheduler::add(Thread *_thread)
+{
   Console::puts("RRScheduler::add() - start.\n");
-  Thread_List* new_thread = (Thread_List*)(MEMORY_POOL->allocate(sizeof(Thread_List)));
-  new_thread->thread=_thread;
-  new_thread->next=NULL;
-  new_thread->prev=NULL;
-  if(head==NULL && current==NULL){
-    head=new_thread;
-    current=new_thread;
+  Thread_List *new_thread = (Thread_List *)(MEMORY_POOL->allocate(sizeof(Thread_List)));
+  new_thread->thread = _thread;
+  new_thread->next = NULL;
+  new_thread->prev = NULL;
+  if (head == NULL && current == NULL)
+  {
+    head = new_thread;
+    current = new_thread;
   }
-  else{
-    current->next=new_thread;
-    new_thread->prev=current;
-    current=new_thread;
+  else
+  {
+    current->next = new_thread;
+    new_thread->prev = current;
+    current = new_thread;
   }
   Console::puts("Thread Added : ");
-  Console::puti(_thread->ThreadId()+1);
+  Console::puti(_thread->ThreadId() + 1);
   Console::puts("\n");
   Console::puts("RRScheduler::add() - end.\n");
 }
 
-void RRScheduler::resume(Thread * _thread) {
+void RRScheduler::resume(Thread *_thread)
+{
   Console::puts("RRScheduler::resume() - start.\n");
   add(_thread);
   Console::puts("Thread Resume:");
-  Console::putui(_thread->ThreadId()+1);
+  Console::putui(_thread->ThreadId() + 1);
   Console::puts("\n");
   Console::puts("RRScheduler::resume() - end.\n");
 }
 
-void RRScheduler::terminate(Thread * _thread) {
+void RRScheduler::terminate(Thread *_thread)
+{
   Console::puts("RRScheduler::terminate() - start.\n");
-  if(Thread::CurrentThread()==_thread){
+  if (Thread::CurrentThread() == _thread)
+  {
     yield();
   }
-  else if (head->thread==_thread){
-    head=head->next;
-    head->prev=NULL;
+  else if (head->thread == _thread)
+  {
+    head = head->next;
+    head->prev = NULL;
   }
-  else{
-    Thread_List* itr =head;    
-     for(itr=head;itr->next->thread!=_thread;itr=itr->next)
-     {
+  else
+  {
+    Thread_List *itr = head;
+    for (itr = head; itr->next->thread != _thread; itr = itr->next)
+    {
+    }
 
-     }
+    if (itr != NULL)
+    {
 
-     if(itr!=NULL){
+      Thread_List *n_temp = itr->next;
+      Thread_List *p_temp = itr->prev;
+      itr->next = n_temp->next;
+      itr->prev = p_temp->prev;
 
-    Thread_List* n_temp = itr->next;    
-    Thread_List* p_temp = itr->prev;    
-    itr->next=n_temp->next;
-    itr->prev=p_temp->prev;
-
-  MEMORY_POOL->release((unsigned long)n_temp);
-  MEMORY_POOL->release((unsigned long)p_temp);
-     }
+      MEMORY_POOL->release((unsigned long)n_temp);
+      MEMORY_POOL->release((unsigned long)p_temp);
+    }
   }
   Console::puts("Thread Terminated : ");
-  Console::puti(_thread->ThreadId()+1);
+  Console::puti(_thread->ThreadId() + 1);
   Console::puts("\n");
   Console::puts("RRScheduler::terminate() - end.\n");
 }
 
-
-void RRScheduler::quantum_manager() {
+void RRScheduler::quantum_manager()
+{
   Console::puts("RRScheduler::quantum_manager() - start.\n");
   Console::puts("One Quantum is over.\n");
-  quantum_passed=true;
+  quantum_passed = true;
   /* Send an EOI message to the master interrupt controller. */
   Machine::outportb(0x20, 0x20);
   resume(Thread::CurrentThread());
--- MP5/simple_timer.C
+++ MP5/simple_timer.C
@@ -1,4 +1,4 @@
-/* 
+/*
     File: simple_timer.C
 
     Author: R. Bettati
@@ -6,14 +6,14 @@
             Texas A&M University
     Date  : 09/03/19
 
-    Simple control of the 
+    Simple control of the
 */
 
 /*--------------------------------------------------------------------------*/
 /* DEFINES */
 /*--------------------------------------------------------------------------*/
 
-    /* -- (none) -- */
+/* -- (none) -- */
 
 /*--------------------------------------------------------------------------*/
 /* INCLUDES */
@@ -32,122 +32,124 @@ extern Scheduler *SYSTEM_SCHEDULER;
 /* CONSTRUCTOR */
 /*--------------------------------------------------------------------------*/
 
-SimpleTimer::SimpleTimer(int _hz) {
+SimpleTimer::SimpleTimer(int _hz)
+{
   /* How long has the system been running? */
-  seconds =  0; 
-  ticks   =  0; /* ticks since last "seconds" update.    */
+  seconds = 0;
+  ticks = 0; /* ticks since last "seconds" update.    */
 
   /* At what frequency do we update the ticks counter? */
   /* hz      = 18; */
-                /* Actually, by defaults it is 18.22Hz.
-                   In this way, a 16-bit counter wraps
-                   around every hour.                    */
+  /* Actually, by defaults it is 18.22Hz.
+     In this way, a 16-bit counter wraps
+     around every hour.                    */
   set_frequency(_hz);
-
 }
 
 /*--------------------------------------------------------------------------*/
 /* METHODS FOR CLASS   S i m p l e T i m e r */
 /*--------------------------------------------------------------------------*/
 
+void SimpleTimer::handle_interrupt(REGS *_r)
+{
+  /* What to do when timer interrupt occurs? In this case, we update "ticks",
+     and maybe update "seconds".
+     This must be installed as the interrupt handler for the timer in the
+     when the system gets initialized. (e.g. in "kernel.C") */
 
-void SimpleTimer::handle_interrupt(REGS *_r) {
-/* What to do when timer interrupt occurs? In this case, we update "ticks",
-   and maybe update "seconds".
-   This must be installed as the interrupt handler for the timer in the 
-   when the system gets initialized. (e.g. in "kernel.C") */
+  /* Increment our "ticks" count */
+  ticks++;
 
-    /* Increment our "ticks" count */
-    ticks++;
-
-    /* Whenever a second is over, we update counter accordingly. */
-    if (ticks >= hz )
-    {
-        seconds++;
-        ticks = 0;
-        Console::puts("One second has passed\n");
-    }
+  /* Whenever a second is over, we update counter accordingly. */
+  if (ticks >= hz)
+  {
+    seconds++;
+    ticks = 0;
+    Console::puts("One second has passed\n");
+  }
 }
 
+void SimpleTimer::set_frequency(int _hz)
+{
+  /* Set the interrupt frequency for the simple timer.
+     Preferably set this before installing the timer handler!                 */
 
-void SimpleTimer::set_frequency(int _hz) {
-/* Set the interrupt frequency for the simple timer.
-   Preferably set this before installing the timer handler!                 */
-
-    hz = _hz;                            /* Remember the frequency.           */
-    int divisor = 1193180 / _hz;         /* The input clock runs at 1.19MHz   */
-    Machine::outportb(0x43, 0x34);                /* Set command byte to be 0x36.      */
-    Machine::outportb(0x40, divisor & 0xFF);      /* Set low byte of divisor.          */
-    Machine::outportb(0x40, divisor >> 8);        /* Set high byte of divisor.         */
+  hz = _hz;                                /* Remember the frequency.           */
+  int divisor = 1193180 / _hz;             /* The input clock runs at 1.19MHz   */
+  Machine::outportb(0x43, 0x34);           /* Set command byte to be 0x36.      */
+  Machine::outportb(0x40, divisor & 0xFF); /* Set low byte of divisor.          */
+  Machine::outportb(0x40, divisor >> 8);   /* Set high byte of divisor.         */
 }
 
-void SimpleTimer::current(unsigned long * _seconds, int * _ticks) {
-/* Return the current "time" since the system started. */
+void SimpleTimer::current(unsigned long *_seconds, int *_ticks)
+{
+  /* Return the current "time" since the system started. */
 
   *_seconds = seconds;
-  *_ticks   = ticks;
+  *_ticks = ticks;
 }
 
-void SimpleTimer::wait(unsigned long _seconds) {
-/* Wait for a particular time to be passed. This is based on busy looping! */
+void SimpleTimer::wait(unsigned long _seconds)
+{
+  /* Wait for a particular time to be passed. This is based on busy looping! */
 
-    unsigned long now_seconds;
-    int           now_ticks;
-    current(&now_seconds, &now_ticks);
+  unsigned long now_seconds;
+  int now_ticks;
+  current(&now_seconds, &now_ticks);
 
-    unsigned long then_seconds = now_seconds + _seconds;
+  unsigned long then_seconds = now_seconds + _seconds;
 
-    while((seconds <= then_seconds) && (ticks < now_ticks));
+  while ((seconds <= then_seconds) && (ticks < now_ticks))
+    ;
 }
-
 
 /*--------------------------------------------------------------------------*/
 /* METHODS FOR CLASS   EOQ Timer */
 /*--------------------------------------------------------------------------*/
-EOQTimer::EOQTimer(int _hz): SimpleTimer(_hz){
-/* How long has the system been running? */
-Console::puts("EOQTimer Constructor - Start\n");
-hz=_hz;
-Console::puts("EOQTimer Constructor - End\n");
+EOQTimer::EOQTimer(int _hz) : SimpleTimer(_hz)
+{
+  /* How long has the system been running? */
+  Console::puts("EOQTimer Constructor - Start\n");
+  hz = _hz;
+  Console::puts("EOQTimer Constructor - End\n");
 }
 
-void EOQTimer::handle_interrupt(REGS *_r) {
-/* What to do when timer interrupt occurs? In this case, we update "ticks",
-   and maybe update "seconds".
-   This must be installed as the interrupt handler for the timer in the 
-   when the system gets initialized. (e.g. in "kernel.C") */
+void EOQTimer::handle_interrupt(REGS *_r)
+{
+  /* What to do when timer interrupt occurs? In this case, we update "ticks",
+     and maybe update "seconds".
+     This must be installed as the interrupt handler for the timer in the
+     when the system gets initialized. (e.g. in "kernel.C") */
 
-    /* Increment our "ticks" count */
-    ticks++;
+  /* Increment our "ticks" count */
+  ticks++;
 
-    /*
-    qunatum is 50 ms
-    1 s =1000 ms
-    which  means 50 ms = hz/20 
-    Whenever a second is over, we update counter accordingly. 
-    */
-    if (ticks >= hz/20 )
-    {
-        ((RRScheduler*) SYSTEM_SCHEDULER)->quantum_manager();
-        reset_timer_counter();
-        Console::puts("Quantum has passed\n");
-    }
+  /*
+  qunatum is 50 ms
+  1 s =1000 ms
+  which  means 50 ms = hz/20
+  Whenever a second is over, we update counter accordingly.
+  */
+  if (ticks >= hz / 20)
+  {
+    ((RRScheduler *)SYSTEM_SCHEDULER)->quantum_manager();
+    reset_timer_counter();
+    Console::puts("Quantum has passed\n");
+  }
 }
-    void EOQTimer::reset_timer_counter()
-    {
+void EOQTimer::reset_timer_counter()
+{
   /*
     reset the timer when thread is yielded or time quantum is over
   */
-        ticks =0;
-        }
+  ticks = 0;
+}
 
-  int EOQTimer::get_timer_counter()
-  {
+int EOQTimer::get_timer_counter()
+{
 
   /*
     get the timer counter
   */
-    return ticks;
-
-  }
-
+  return ticks;
+}
