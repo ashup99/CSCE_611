bbfee8cf45943843de10de14ffa2e5fb03411957
Ashutosh Punyani <ashutoshspunyani99@tamu.edu>
2023-11-15 21:36:01 -0600

    blocking disk changes added and threadc reverted back

--- MP6/blocking_disk.C
+++ MP6/blocking_disk.C
@@ -1,8 +1,8 @@
 /*
      File        : blocking_disk.c
 
-     Author      : 
-     Modified    : 
+     Author      : Ashutosh Punyani
+     Modified    : November 13, 2023
 
      Description : 
 
@@ -22,6 +22,9 @@
 #include "utils.H"
 #include "console.H"
 #include "blocking_disk.H"
+#include "scheduler.H"
+
+extern Scheduler* SYSTEM_SCHEDULER;
 
 /*--------------------------------------------------------------------------*/
 /* CONSTRUCTOR */
@@ -29,6 +32,8 @@
 
 BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size) 
   : SimpleDisk(_disk_id, _size) {
+  Console::puts("Constructed BlockingDisk::BlockingDisk() - start.\n");
+  Console::puts("Constructed BlockingDisk::BlockingDisk() - end.\n");
 }
 
 /*--------------------------------------------------------------------------*/
@@ -36,13 +41,25 @@ BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size)
 /*--------------------------------------------------------------------------*/
 
 void BlockingDisk::read(unsigned long _block_no, unsigned char * _buf) {
-  // -- REPLACE THIS!!!
+  Console::puts("BlockingDisk::read() - start.\n");
   SimpleDisk::read(_block_no, _buf);
+  Console::puts("BlockingDisk::read() - end.\n");
 
 }
 
-
 void BlockingDisk::write(unsigned long _block_no, unsigned char * _buf) {
-  // -- REPLACE THIS!!!
+  Console::puts("BlockingDisk::write() - start.\n");
   SimpleDisk::write(_block_no, _buf);
+  Console::puts("BlockingDisk::write() - end.\n");
+}
+
+bool BlockingDisk::is_ready_blocked(){
+  return SimpleDisk::is_ready();
+}
+
+void BlockingDisk::wait_until_ready(){
+  while(!SimpleDisk::is_ready()){
+    SYSTEM_SCHEDULER->resume(Thread::CurrentThread());
+    SYSTEM_SCHEDULER->yield();
+  }
 }
